675f11b4f948acb65859da0a3913c3386009c619
thefarside <131199533+thefarside@users.noreply.github.com>
2025-10-29 15:54:37 -0700

    Update class-profile-contact-website.php
    Removed field from /wp-admin/user-new.php

--- examples/modules/class-profile-contact-website.php
+++ examples/modules/class-profile-contact-website.php
@@ -10,11 +10,18 @@ class Profile_Contact_Website {
 	public static function initialize() : void {
 		add_filter( 'parse_html-profile.php', array( static::class, 'remove_profile_field' ), PHP_INT_MAX, 2 );
 		add_filter( 'parse_html-user-edit.php', array( static::class, 'remove_profile_field' ), PHP_INT_MAX, 2 );
+		add_filter( 'parse_html-user-new.php', array( static::class, 'remove_new_user_field' ), PHP_INT_MAX, 2 );
 		return;
 	}
 
 	public static function remove_profile_field( DOMDocument $document, DOMXPath $selector ) : DOMDocument {
 		$settings = $selector->querySelector( '//tr[@class = "user-url-wrap"]' );
+		$settings?->remove();
+		return $document;
+	}
+	
+	public static function remove_new_user_field( DOMDocument $document, DOMXPath $selector ) : DOMDocument {
+		$settings = $selector->querySelector( '//tr[.//input[@id = "url"]]' );
 		$settings?->remove();
 		return $document;
 	}
