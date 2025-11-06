5381459477f1cec70fe59d54b27bb5a5972d3425
thefarside <131199533+thefarside@users.noreply.github.com>
2025-11-06 11:36:32 -0800

    Fixed missing WC class error
    For when WC isn't active/installed but the module is.

--- examples/modules/wc/class-wc-emails-payment-method.php
+++ examples/modules/wc/class-wc-emails-payment-method.php
@@ -8,9 +8,11 @@ class WC_Emails_Payment_Method {
 
 	public static function initialize() : void {
 		add_filter( 'pre_wp_mail', array( static::class, 'disable_email_notification' ), PHP_INT_MAX, 2 );
+		return;
 	}
 
 	public static function disable_email_notification( null $bool, array $atts ) : null|bool {
+		if ( ! class_exists( 'WC_Payment_Gateways' ) ) return null;
 		if ( '' !== get_option( 'blogname' ) ) {
 			$site_title = wp_specialchars_decode( get_option( 'blogname' ), ENT_QUOTES );
 		} else {
